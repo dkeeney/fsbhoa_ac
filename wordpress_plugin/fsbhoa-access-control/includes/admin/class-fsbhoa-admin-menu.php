98d92924d28b47c921436db89b7edb8b47730304
dkeeney <pi@NAS.local>
2025-05-30 12:05:54 -0700

    feat: Implement sticky form for Add New Cardholder

--- wordpress_plugin/fsbhoa-access-control/includes/admin/class-fsbhoa-admin-menu.php
+++ wordpress_plugin/fsbhoa-access-control/includes/admin/class-fsbhoa-admin-menu.php
@@ -147,32 +147,74 @@ class Fsbhoa_Admin_Menu {
      * @since 0.1.1
      * @param string $action Current action ('add' or 'edit')
      */
+    // We can add methods here later for enqueueing admin scripts and styles
+    // public function enqueue_styles() { ... }
+    // public function enqueue_scripts() { ... }
+
+
     public function render_add_new_cardholder_form($action = 'add') {
-        // Security check: Nonce verification will be added here for POST requests.
-        // Data saving logic will also go here.
+        $form_data = array( // Initialize $form_data
+            // 'rfid_id'    => '', // REMOVE THIS LINE
+            'first_name' => '',
+            'last_name'  => '',
+            // Add other fields here as they are added to the form
+        );
+        $errors = array(); // Initialize $errors
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit_add_cardholder'])) {
-            // Verify nonce (important for security)
             if (isset($_POST['fsbhoa_add_cardholder_nonce']) && wp_verify_nonce(sanitize_text_field(wp_unslash($_POST['fsbhoa_add_cardholder_nonce'])), 'fsbhoa_add_cardholder_action')) {
-                // Nonce is valid, process the data
-                echo '<div id="message" class="updated notice is-dismissible"><p>Form submitted! (Data processing not yet implemented)</p><pre>';
-                // Sanitize and display POST data (for debugging purposes only)
-                // In a real application, you would save this data to the database.
-                print_r(array_map('sanitize_text_field', wp_unslash($_POST)));
-                echo '</pre></div>';
+                
+                // Sanitize and collect form data into $form_data
+                // $form_data['rfid_id']    = isset($_POST['rfid_id']) ? sanitize_text_field(wp_unslash($_POST['rfid_id'])) : ''; // REMOVE THIS LINE
+                $form_data['first_name'] = isset($_POST['first_name']) ? sanitize_text_field(wp_unslash($_POST['first_name'])) : '';
+                $form_data['last_name']  = isset($_POST['last_name']) ? sanitize_text_field(wp_unslash($_POST['last_name'])) : '';
+
+                // ** START VALIDATION LOGIC **
+                $errors = array(); // Re-initialize errors for this submission
+
+                // Validate RFID ID // ENTIRE BLOCK TO BE REMOVED
+                // if (empty($form_data['rfid_id'])) {
+                //     $errors['rfid_id'] = __( 'RFID ID is required.', 'fsbhoa-ac' );
+                // } elseif (!ctype_digit($form_data['rfid_id'])) { 
+                //     $errors['rfid_id'] = __( 'RFID ID must be numeric.', 'fsbhoa-ac' );
+                // } elseif (strlen($form_data['rfid_id']) !== 8) { 
+                //     $errors['rfid_id'] = __( 'RFID ID must be 8 digits.', 'fsbhoa-ac' );
+                // }
+                // END RFID VALIDATION BLOCK REMOVAL
+
+                // Validate First Name
+                if (empty($form_data['first_name'])) {
+                    $errors['first_name'] = __( 'First Name is required.', 'fsbhoa-ac' );
+                } elseif (strlen($form_data['first_name']) > 100) {
+                    $errors['first_name'] = __( 'First Name is too long (max 100 characters).', 'fsbhoa-ac' );
+                }
+
+                // Validate Last Name
+                if (empty($form_data['last_name'])) {
+                    $errors['last_name'] = __( 'Last Name is required.', 'fsbhoa-ac' );
+                } elseif (strlen($form_data['last_name']) > 100) {
+                    $errors['last_name'] = __( 'Last Name is too long (max 100 characters).', 'fsbhoa-ac' );
+                }
+
+                if (empty($errors)) {
+                    echo '<div id="message" class="updated notice is-dismissible"><p>' . esc_html__('Validation successful! Data ready for saving (not yet implemented).', 'fsbhoa-ac') . '</p><pre>';
+                    print_r($form_data); // Display sanitized data
+                    echo '</pre></div>';
+                    $form_data = array_fill_keys(array_keys($form_data), ''); 
+                } else {
+                    echo '<div id="message" class="error notice is-dismissible"><p>' . esc_html__('Please correct the errors below and try again.', 'fsbhoa-ac') . '</p>';
+                    foreach ($errors as $field => $error_message) {
+                       echo '<p><strong>' . esc_html(ucfirst(str_replace('_', ' ', $field))) . ':</strong> ' . esc_html($error_message) . '</p>';
+                    }
+                    echo '</div>';
+                }
             } else {
-                // Nonce is invalid
                 echo '<div id="message" class="error notice is-dismissible"><p>' . esc_html__('Security check failed. Please try again.', 'fsbhoa-ac') . '</p></div>';
             }
         }
         ?>
         <div class="wrap">
-            <h1>
-                <?php
-                // TODO: Add logic for 'edit' mode title later
-                echo esc_html__( 'Add New Cardholder', 'fsbhoa-ac' );
-                ?>
-            </h1>
+            <h1><?php echo esc_html__( 'Add New Cardholder', 'fsbhoa-ac' ); ?></h1>
 
             <form method="POST" action="?page=fsbhoa_ac_cardholders&action=add">
                 <?php wp_nonce_field( 'fsbhoa_add_cardholder_action', 'fsbhoa_add_cardholder_nonce' ); ?>
@@ -181,19 +223,11 @@ class Fsbhoa_Admin_Menu {
                     <tbody>
                         <tr>
                             <th scope="row">
-                                <label for="rfid_id"><?php esc_html_e( 'RFID ID', 'fsbhoa-ac' ); ?></label>
-                            </th>
-                            <td>
-                                <input type="text" name="rfid_id" id="rfid_id" class="regular-text" value="" required>
-                                <p class="description"><?php esc_html_e( 'Enter the 8-digit RFID card ID.', 'fsbhoa-ac' ); ?></p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">
                                 <label for="first_name"><?php esc_html_e( 'First Name', 'fsbhoa-ac' ); ?></label>
                             </th>
                             <td>
-                                <input type="text" name="first_name" id="first_name" class="regular-text" value="" required>
+                                <input type="text" name="first_name" id="first_name" class="regular-text" 
+                                       value="<?php echo esc_attr($form_data['first_name']); ?>" required>
                             </td>
                         </tr>
                         <tr>
@@ -201,22 +235,18 @@ class Fsbhoa_Admin_Menu {
                                 <label for="last_name"><?php esc_html_e( 'Last Name', 'fsbhoa-ac' ); ?></label>
                             </th>
                             <td>
-                                <input type="text" name="last_name" id="last_name" class="regular-text" value="" required>
+                                <input type="text" name="last_name" id="last_name" class="regular-text" 
+                                       value="<?php echo esc_attr($form_data['last_name']); ?>" required>
                             </td>
                         </tr>
                         </tbody>
                 </table>
-
-                <?php submit_button( __( 'Add Cardholder', 'fsbhoa-ac' ), 'primary', 'submit_add_cardholder' ); ?>
+                <?php submit_button( __( 'Save Basic Info & Proceed to Photo', 'fsbhoa-ac' ), 'primary', 'submit_add_cardholder' ); // Changed button text ?>
             </form>
             <p><a href="?page=fsbhoa_ac_cardholders"><?php esc_html_e( '&larr; Back to Cardholders List', 'fsbhoa-ac' ); ?></a></p>
         </div>
         <?php
-    }
-    // We can add methods here later for enqueueing admin scripts and styles
-    // public function enqueue_styles() { ... }
-    // public function enqueue_scripts() { ... }
-
-} // end class Fsbhoa_Admin_Menu
+    }  // end function
+} // end class
 
 ?>
